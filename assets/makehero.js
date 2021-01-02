2666a214121b8a5c77f86bacc93d0c0da210c5c5
broop <soundwanders@gmail.com>
2021-01-02 17:26:09 -0500

    removed buttons div container, added class to group buttons so add/subtract stats on click remains functional

--- assets/makehero.js
+++ assets/makehero.js
@@ -40,6 +40,7 @@ let luck = 0;
   const luckFrame = document.getElementById("lukFrame");
   luckFrame.addEventListener('click', addLuk);
   luckFrame.addEventListener('contextmenu', subtractLuk);
+//
 
 // ## Add Event Listeners to Stat Buttons ##
 
@@ -82,189 +83,192 @@ let luck = 0;
   // Decrease Luck Button
     const downLuk = document.getElementById("downLuk");
     downLuk.addEventListener('click', subtractLuk);
+//
 
 // --- FUNCTIONS ---
 // ## Add Stats ##
 
-// Add Strength
-function addStr() {
-  // get hidden strength stat bar div
-  // hidden until user adds stats, then becomes visible
-  let strengthBar = document.getElementById("strBar");
+  // Add Strength
+  function addStr() {
+    // get hidden strength stat bar div
+    // hidden until user adds stats, then becomes visible
+    let strengthBar = document.getElementById("strBar");
 
-  // while width is less than max width (30%) , add increments of 3
-  // and change width, background color of the empty strength div
-  // to represent the increase of strength stat
-  while (strength < 30) {
-    strength += 3;
-    strengthBar.style.width = strength + "%" ,
-    strengthBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Increased Strength");
-    console.log(strength);
-    return;
-  }
-  // if strength is 30 (max width) prevent additional strength
-  if (strength >= 30) {
-    console.log("Maxed Strength");
-  }
-};
+    // while width is less than max width (30%) , add increments of 3
+    // and change width, background color of the empty strength div
+    // to represent the increase of strength stat
+    while (strength < 30) {
+      strength += 3;
+      strengthBar.style.width = strength + "%" ,
+      strengthBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Increased Strength");
+      console.log(strength);
+      return;
+    }
+    // if strength is 30 (max width) prevent additional strength
+    if (strength >= 30) {
+      console.log("Maxed Strength");
+    }
+  };
 
-// Add Vitality
-function addVit() {
-  let vitalityBar = document.getElementById("vitBar");
+  // Add Vitality
+  function addVit() {
+    let vitalityBar = document.getElementById("vitBar");
 
-  while (vitality < 30) {
-    vitality += 3;
-    vitalityBar.style.width = vitality + "%" ,
-    vitalityBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Increased Vitality");
-    console.log(vitality);
-    return;
-  }
-  if (vitality >= 30) {
-    console.log("Maxed Vitality");
-  }
-};
+    while (vitality < 30) {
+      vitality += 3;
+      vitalityBar.style.width = vitality + "%" ,
+      vitalityBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Increased Vitality");
+      console.log(vitality);
+      return;
+    }
+    if (vitality >= 30) {
+      console.log("Maxed Vitality");
+    }
+  };
 
-// Add Agility
-function addAgi() {
-  let agilityBar = document.getElementById("agiBar");
+  // Add Agility
+  function addAgi() {
+    let agilityBar = document.getElementById("agiBar");
 
-  while (agility < 30) {
-    agility += 3;
-    agilityBar.style.width = agility + "%" ,
-    agilityBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Increased Agility");
-    console.log(agility);
-    return;
-  }
-  if (agility >= 30) {
-    console.log("Maxed Agility");
-  }
-};
+    while (agility < 30) {
+      agility += 3;
+      agilityBar.style.width = agility + "%" ,
+      agilityBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Increased Agility");
+      console.log(agility);
+      return;
+    }
+    if (agility >= 30) {
+      console.log("Maxed Agility");
+    }
+  };
 
-// Add Intelligence
-function addInt() {
-  let intelligenceBar = document.getElementById("intBar");
+  // Add Intelligence
+  function addInt() {
+    let intelligenceBar = document.getElementById("intBar");
 
-  while (intelligence < 30) {
-    intelligence += 3;
-    intelligenceBar.style.width = intelligence + "%" ,
-    intelligenceBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Increased Intelligence");
-    console.log(intelligence);
-    return;
-  }
-  if (intelligence >= 30) {
-    console.log("Maxed Intelligence");
-  }
-};
+    while (intelligence < 30) {
+      intelligence += 3;
+      intelligenceBar.style.width = intelligence + "%" ,
+      intelligenceBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Increased Intelligence");
+      console.log(intelligence);
+      return;
+    }
+    if (intelligence >= 30) {
+      console.log("Maxed Intelligence");
+    }
+  };
 
-// Add Luck
-function addLuk() {
-  let luckBar = document.getElementById("lukBar");
+  // Add Luck
+  function addLuk() {
+    let luckBar = document.getElementById("lukBar");
 
-  while (luck < 30) {
-    luck += 3;
-    luckBar.style.width = luck + "%" ,
-    luckBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Increased Luck");
-    console.log(luck);
-    return;
-  }
-  if (luck >= 30) {
-    console.log("Maxed Luck");
-  }
-};
+    while (luck < 30) {
+      luck += 3;
+      luckBar.style.width = luck + "%" ,
+      luckBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Increased Luck");
+      console.log(luck);
+      return;
+    }
+    if (luck >= 30) {
+      console.log("Maxed Luck");
+    }
+  };
+//
 
 // ## Subtract Stats ##
 
-// Subtract Strength
-function subtractStr(e) {
-  let strengthBar = document.getElementById("strBar");
-  e.preventDefault();
+  // Subtract Strength
+  function subtractStr(e) {
+    let strengthBar = document.getElementById("strBar");
+    e.preventDefault();
 
-  while (strength > 0) {
-    strength -= 3;
-    strengthBar.style.width = strength + "%" ,
-    strengthBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Decreased Strength");
-    console.log(strength);
-    return;
-  }
-  if (strength <= 0) {
-    console.log("Zero Strength");
-  }
-};
+    while (strength > 0) {
+      strength -= 3;
+      strengthBar.style.width = strength + "%" ,
+      strengthBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Decreased Strength");
+      console.log(strength);
+      return;
+    }
+    if (strength <= 0) {
+      console.log("Zero Strength");
+    }
+  };
 
-// Subtract Vitality
-function subtractVit(e) {
-  let vitalityBar = document.getElementById("vitBar");
-  e.preventDefault();
+  // Subtract Vitality
+  function subtractVit(e) {
+    let vitalityBar = document.getElementById("vitBar");
+    e.preventDefault();
 
-  while (vitality > 0) {
-    vitality-= 3;
-    vitalityBar.style.width = vitality + "%" ,
-    vitalityBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Decreased Vitality");
-    console.log(vitality);
-    return;
-  }
-  if (vitality <= 0) {
-    console.log("Zero Vitality");
-  }
-};
+    while (vitality > 0) {
+      vitality-= 3;
+      vitalityBar.style.width = vitality + "%" ,
+      vitalityBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Decreased Vitality");
+      console.log(vitality);
+      return;
+    }
+    if (vitality <= 0) {
+      console.log("Zero Vitality");
+    }
+  };
 
-// Subtract Agility
-function subtractAgi(e) {
-  let agilityBar = document.getElementById("agiBar");
-  e.preventDefault();
+  // Subtract Agility
+  function subtractAgi(e) {
+    let agilityBar = document.getElementById("agiBar");
+    e.preventDefault();
 
-  while (agility > 0) {
-    agility -= 3;
-    agilityBar.style.width = agility + "%" ,
-    agilityBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Decreased Agility");
-    console.log(agility);
-    return;
-  }
-  if (agility <= 0) {
-    console.log("Zero Agility");
-  }
-};
+    while (agility > 0) {
+      agility -= 3;
+      agilityBar.style.width = agility + "%" ,
+      agilityBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Decreased Agility");
+      console.log(agility);
+      return;
+    }
+    if (agility <= 0) {
+      console.log("Zero Agility");
+    }
+  };
 
-// Subtract Intelligence
-function subtractInt(e) {
-  let intelligenceBar = document.getElementById("intBar");
-  e.preventDefault();
+  // Subtract Intelligence
+  function subtractInt(e) {
+    let intelligenceBar = document.getElementById("intBar");
+    e.preventDefault();
 
-  while (intelligence > 0) {
-    intelligence -= 3;
-    intelligenceBar.style.width = intelligence + "%" ,
-    intelligenceBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Decreased Intelligence");
-    console.log(intelligence);
-    return;
-  }
-  if (intelligence <= 0) {
-    console.log("Zero Intelligence");
-  }
-};
+    while (intelligence > 0) {
+      intelligence -= 3;
+      intelligenceBar.style.width = intelligence + "%" ,
+      intelligenceBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Decreased Intelligence");
+      console.log(intelligence);
+      return;
+    }
+    if (intelligence <= 0) {
+      console.log("Zero Intelligence");
+    }
+  };
 
-// Subtract Luck
-function subtractLuk(e) {
-  let luckBar = document.getElementById("lukBar");
-  e.preventDefault();
+  // Subtract Luck
+  function subtractLuk(e) {
+    let luckBar = document.getElementById("lukBar");
+    e.preventDefault();
 
-  while (luck > 0) {
-    luck -= 3;
-    luckBar.style.width = luck + "%" ,
-    luckBar.style.backgroundColor = "rgb(180, 92, 173)";
-    console.log("Decreased Luck");
-    console.log(luck);
-    return;
-  }
-  if (luck <= 0) {
-    console.log("Zero Luck");
-  }
-};
+    while (luck > 0) {
+      luck -= 3;
+      luckBar.style.width = luck + "%" ,
+      luckBar.style.backgroundColor = "rgb(180, 92, 173)";
+      console.log("Decreased Luck");
+      console.log(luck);
+      return;
+    }
+    if (luck <= 0) {
+      console.log("Zero Luck");
+    }
+  };
+//
