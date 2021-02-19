3b0336d2df487db2007a9ec8b96176293964f9dd
broop <soundwanders@gmail.com>
2021-02-18 22:18:19 -0500

    refactored stat add/subtract functions. added normalizeCSS. npm update and npm fixes completed.

--- assets/makehero.js
+++ assets/makehero.js
@@ -1,6 +1,6 @@
 // js
 
-// --- STATS ---
+// _________________ STATS _________________  //
 
 // --- Create Empty Variables for Each Stat ---
 // numbers are used to manipulate width of stat bars
@@ -11,325 +11,317 @@ let agility = 0;
 let intelligence = 0;
 let luck = 0;
 
-// --- Add Event Listeners to Stat Bars ---
+// _________________ Add Event Listeners to Stat Bars _________________  //
 
-  // Strength
-  // get empty Strength stat bar frame, serves as skeleton
-  // displays on screen same style as if stats were empty/equal to 0
-  const strengthFrame = document.getElementById("strFrame");
-  // run function addStr to add Strength points when stat bar is clicked
-  strengthFrame.addEventListener('click', addStr);
-  strengthFrame.addEventListener('contextmenu', subtractStr);
+// Strength
+// get empty Strength stat bar frame, serves as skeleton
+// displays on screen same style as if stats were empty/equal to 0
+const strengthFrame = document.getElementById('strFrame');
+// run function addStr to add Strength points when stat bar is clicked
+strengthFrame.addEventListener('click', addStr);
+strengthFrame.addEventListener('contextmenu', subtractStr);
 
-  // Vitality
-  const vitalityFrame = document.getElementById("vitFrame");
-  vitalityFrame.addEventListener('click', addVit);
-  vitalityFrame.addEventListener('contextmenu', subtractVit);
+// Vitality
+const vitalityFrame = document.getElementById('vitFrame');
+vitalityFrame.addEventListener('click', addVit);
+vitalityFrame.addEventListener('contextmenu', subtractVit);
 
-  // Agility
-  const agilityFrame = document.getElementById("agiFrame");
-  agilityFrame.addEventListener('click', addAgi);
-  agilityFrame.addEventListener('contextmenu', subtractAgi);
+// Agility
+const agilityFrame = document.getElementById('agiFrame');
+agilityFrame.addEventListener('click', addAgi);
+agilityFrame.addEventListener('contextmenu', subtractAgi);
 
-  // Intelligence
-  const intelligenceFrame = document.getElementById("intFrame");
-  intelligenceFrame.addEventListener('click', addInt);
-  intelligenceFrame.addEventListener('contextmenu', subtractInt);
+// Intelligence
+const intelligenceFrame = document.getElementById('intFrame');
+intelligenceFrame.addEventListener('click', addInt);
+intelligenceFrame.addEventListener('contextmenu', subtractInt);
 
-  // Luck
-  const luckFrame = document.getElementById("lukFrame");
-  luckFrame.addEventListener('click', addLuk);
-  luckFrame.addEventListener('contextmenu', subtractLuk);
+// Luck
+const luckFrame = document.getElementById('lukFrame');
+luckFrame.addEventListener('click', addLuk);
+luckFrame.addEventListener('contextmenu', subtractLuk);
 //
 
 // --- Add Event Listeners to Stat Buttons ---
 
-  // Increase Strength Button
-    const upStr = document.getElementById("upStr");
-    upStr.addEventListener('click', addStr);
+// Increase Strength Button
+const upStr = document.getElementById('upStr');
+upStr.addEventListener('click', addStr);
 
-  // Decrease Strength Button
-    const downStr = document.getElementById("downStr");
-    downStr.addEventListener('click', subtractStr);
+// Decrease Strength Button
+const downStr = document.getElementById('downStr');
+downStr.addEventListener('click', subtractStr);
 
-  // Increase Vitality Button
-    const upVit = document.getElementById("upVit");
-    upVit.addEventListener('click', addVit);
+// Increase Vitality Button
+const upVit = document.getElementById('upVit');
+upVit.addEventListener('click', addVit);
 
-  // Decrease Vitality Button
-    const downVit = document.getElementById("downVit");
-    downVit.addEventListener('click', subtractVit);
+// Decrease Vitality Button
+const downVit = document.getElementById('downVit');
+downVit.addEventListener('click', subtractVit);
 
-  // Increase Agility Button
-    const upAgi = document.getElementById("upAgi");
-    upAgi.addEventListener('click', addAgi);
+// Increase Agility Button
+const upAgi = document.getElementById('upAgi');
+upAgi.addEventListener('click', addAgi);
 
-  // Decrease Agility Button
-    const downAgi = document.getElementById("downAgi");
-    downAgi.addEventListener('click', subtractAgi);
+// Decrease Agility Button
+const downAgi = document.getElementById('downAgi');
+downAgi.addEventListener('click', subtractAgi);
 
-  // Increase Intelligence Button
-    const upInt = document.getElementById("upInt");
-    upInt.addEventListener('click', addInt);
+// Increase Intelligence Button
+const upInt = document.getElementById('upInt');
+upInt.addEventListener('click', addInt);
 
-  // Decrease Intelligence Button
-    const downInt = document.getElementById("downInt");
-    downInt.addEventListener('click', subtractInt);
+// Decrease Intelligence Button
+const downInt = document.getElementById('downInt');
+downInt.addEventListener('click', subtractInt);
 
-  // Increase Luck Button
-    const upLuk = document.getElementById("upLuk");
-    upLuk.addEventListener('click', addLuk);
+// Increase Luck Button
+const upLuk = document.getElementById('upLuk');
+upLuk.addEventListener('click', addLuk);
 
-  // Decrease Luck Button
-    const downLuk = document.getElementById("downLuk");
-    downLuk.addEventListener('click', subtractLuk);
+// Decrease Luck Button
+const downLuk = document.getElementById('downLuk');
+downLuk.addEventListener('click', subtractLuk);
 //
 
-// --- FUNCTIONS ---
+// _________________FUNCTIONS_________________ //
 // --- Add Stats ---
 
-  // Add Strength
-  function addStr() {
-    // get empty strength stat bar div
-    // bar remains "empty" until user adds stats
-    let strengthBar = document.getElementById("strBar");
+// Add Strength
+function addStr () {
+  // get empty strength stat bar div
+  // bar remains "empty" until user adds stats
+  const strengthBar = document.getElementById('strBar');
 
-    // while width is less than max width (30%) , add increments of 3
-    // and change display, width, background color of the strength bar
-    // to represent the strength stat being increased
-    while (strength < 30) {
-      strength += 3;
-      strengthBar.style.width = strength + "%" ,
-      strengthBar.style.display = "inline";
-      strengthBar.style.backgroundImage = "linear-gradient(#ffee10, #fff570)" ,
-      strengthBar.style.opacity = "80%";
-      console.log("Increased Strength");
-      console.log(strength);
-      return;
-    }
-    // if strength is 30 (max width) prevent additional strength
-    if (strength >= 30) {
-      console.log("Maxed Strength");
-    }
-  };
+  // if strength is 30 (max width) prevent additional strength
+  if (strength >= 30) {
+    strength = 30;
+    console.log('Maxed Strength');
+    return;
+  }
 
-  // Add Vitality
-  function addVit() {
-    let vitalityBar = document.getElementById("vitBar");
+  // if width is less than max width (30%) , add increments of 3
+  // and change display, width, background color of the strength bar
+  // to represent the strength stat being increased
+  strength += 3;
+  strengthBar.style.width = strength + '%';
+  strengthBar.style.display = 'inline';
+  strengthBar.style.backgroundImage = 'linear-gradient(#ffee10, #fff570)';
+  strengthBar.style.opacity = '80%';
+  console.log('Increased Strength');
+  console.log(strength);
+}
 
-    while (vitality < 30) {
-      vitality += 3;
-      vitalityBar.style.width = vitality + "%" ,
-      vitalityBar.style.backgroundImage = "linear-gradient(#ffee10, #fff570)" ,
-      vitalityBar.style.opacity = "80%";
-      console.log("Increased Vitality");
-      console.log(vitality);
-      return;
-    }
-    if (vitality >= 30) {
-      console.log("Maxed Vitality");
-    }
-  };
+// Add Vitality
+function addVit () {
+  const vitalityBar = document.getElementById('vitBar');
 
-  // Add Agility
-  function addAgi() {
-    let agilityBar = document.getElementById("agiBar");
+  if (vitality >= 30) {
+    vitality = 30;
+    console.log('Maxed Vitality');
+    return;
+  }
 
-    while (agility < 30) {
-      agility += 3;
-      agilityBar.style.width = agility + "%" ,
-      agilityBar.style.backgroundImage = "linear-gradient(#ffee10, #fff570)" ,
-      agilityBar.style.opacity = "80%";
-      console.log("Increased Agility");
-      console.log(agility);
-      return;
-    }
-    if (agility >= 30) {
-      console.log("Maxed Agility");
-    }
-  };
+  vitality += 3;
+  vitalityBar.style.width = vitality + '%';
+  vitalityBar.style.backgroundImage = 'linear-gradient(#ffee10, #fff570)';
+  vitalityBar.style.opacity = '80%';
+  console.log('Increased Vitality');
+  console.log(vitality);
+}
 
-  // Add Intelligence
-  function addInt() {
-    let intelligenceBar = document.getElementById("intBar");
+// Add Agility
+function addAgi () {
+  const agilityBar = document.getElementById('agiBar');
 
-    while (intelligence < 30) {
-      intelligence += 3;
-      intelligenceBar.style.width = intelligence + "%" ,
-      intelligenceBar.style.backgroundImage = "linear-gradient(#ffee10, #fff570)" ,
-      intelligenceBar.style.opacity = "80%";
-      console.log("Increased Intelligence");
-      console.log(intelligence);
-      return;
-    }
-    if (intelligence >= 30) {
-      console.log("Maxed Intelligence");
-    }
-  };
+  if (agility >= 30) {
+    agility = 30;
+    console.log('Maxed Agility');
+    return;
+  }
 
-  // Add Luck
-  function addLuk() {
-    let luckBar = document.getElementById("lukBar");
+  agility += 3;
+  agilityBar.style.width = agility + '%';
+  agilityBar.style.backgroundImage = 'linear-gradient(#ffee10, #fff570)';
+  agilityBar.style.opacity = '80%';
+  console.log('Increased Agility');
+  console.log(agility);
+}
 
-    while (luck < 30) {
-      luck += 3;
-      luckBar.style.width = luck + "%" ,
-      luckBar.style.backgroundImage = "linear-gradient(#ffee10, #fff570)" ,
-      luckBar.style.opacity = "80%";
-      console.log("Increased Luck");
-      console.log(luck);
-      return;
-    }
-    if (luck >= 30) {
-      console.log("Maxed Luck");
-    }
-  };
-//
+// Add Intelligence
+function addInt () {
+  const intelligenceBar = document.getElementById('intBar');
 
-// --- Subtract Stats ---
+  if (intelligence >= 30) {
+    intelligence = 30;
+    console.log('Maxed Intelligence');
+    return;
+  }
 
-  // Subtract Strength
-  function subtractStr(e) {
-    let strengthBar = document.getElementById("strBar");
-    e.preventDefault();
+  intelligence += 3;
+  intelligenceBar.style.width = intelligence + '%';
+  intelligenceBar.style.backgroundImage = 'linear-gradient(#ffee10, #fff570)';
+  intelligenceBar.style.opacity = '80%';
+  console.log('Increased Intelligence');
+  console.log(intelligence);
+}
 
-    while (strength > 0) {
-      strength -= 3;
-      strengthBar.style.display = "inline";
-      strengthBar.style.width = strength + "%" ,
-      strengthBar.style.opacity = "80%";
-      console.log("Decreased Strength");
-      console.log(strength);
-      return;
-    }
-    if (strength == 0) {
-      strengthBar.style.display = "none";
-      console.log("Zero Strength");
-    }
-  };
+// Add Luck
+function addLuk () {
+  const luckBar = document.getElementById('lukBar');
 
-  // Subtract Vitality
-  function subtractVit(e) {
-    let vitalityBar = document.getElementById("vitBar");
-    e.preventDefault();
+  if (luck >= 30) {
+    luck = 30;
+    console.log('Maxed Luck');
+  }
 
-    while (vitality > 0) {
-      vitality-= 3;
-      vitalityBar.style.display = "inline";
-      vitalityBar.style.width = vitality + "%" ,
-      vitalityBar.style.opacity = "80%";
-      console.log("Decreased Vitality");
-      console.log(vitality);
-      return;
-    }
-    if (vitality <= 0) {
-      vitalityBar.style.display = "none";
-      console.log("Zero Vitality");
-    }
-  };
+  luck += 3;
+  luckBar.style.width = luck + '%';
+  luckBar.style.backgroundImage = 'linear-gradient(#ffee10, #fff570)';
+  luckBar.style.opacity = '80%';
+  console.log('Increased Luck');
+  console.log(luck);
+}
 
-  // Subtract Agility
-  function subtractAgi(e) {
-    let agilityBar = document.getElementById("agiBar");
-    e.preventDefault();
+// _________________Subtract Stats _________________ //
 
-    while (agility > 0) {
-      agility -= 3;
-      agilityBar.style.display = "inline";
-      agilityBar.style.width = agility + "%" ,
-      agilityBar.style.opacity = "80%";
-      console.log("Decreased Agility");
-      console.log(agility);
-      return;
-    }
-    if (agility <= 0) {
-      agilityBar.style.display = "none";
-      console.log("Zero Agility");
-    }
-  };
+// Subtract Strength
+function subtractStr (e) {
+  const strengthBar = document.getElementById('strBar');
+  e.preventDefault();
 
-  // Subtract Intelligence
-  function subtractInt(e) {
-    let intelligenceBar = document.getElementById("intBar");
-    e.preventDefault();
+  if (strength <= 0) {
+    strengthBar.style.display = 'none';
+    strength = 0;
+    console.log('Zero Strength');
+    return;
+  }
 
-    while (intelligence > 0) {
-      intelligence -= 3;
-      intelligenceBar.style.display = "inline";
-      intelligenceBar.style.width = intelligence + "%" ,
-      console.log("Decreased Intelligence");
-      console.log(intelligence);
-      return;
-    }
-    if (intelligence <= 0) {
-      intelligenceBar.style.display = "none";
-      console.log("Zero Intelligence");
-    }
-  };
+  strength -= 3;
+  strengthBar.style.display = 'inline';
+  strengthBar.style.width = strength + '%';
+  strengthBar.style.opacity = '80%';
+  console.log('Decreased Strength');
+  console.log(strength);
+}
 
-  // Subtract Luck
-  function subtractLuk(e) {
-    let luckBar = document.getElementById("lukBar");
-    e.preventDefault();
+// Subtract Vitality
+function subtractVit (e) {
+  const vitalityBar = document.getElementById('vitBar');
+  e.preventDefault();
 
-    while (luck > 0) {
-      luck -= 3;
-      luckBar.style.display = "inline";
-      luckBar.style.width = luck + "%" ,
-      console.log("Decreased Luck");
-      console.log(luck);
-      return;
-    }
-    if (luck <= 0) {
-      luckBar.style.display = "none";
-      console.log("Zero Luck");
-    }
-  };
+  if (vitality <= 0) {
+    vitalityBar.style.display = 'none';
+    vitality = 0;
+    console.log('Zero Vitality');
+    return;
+  }
 
-// Change Hero Display Depending on Selected Class
+  vitality -= 3;
+  vitalityBar.style.display = 'inline';
+  vitalityBar.style.width = vitality + '%';
+  vitalityBar.style.opacity = '80%';
+  console.log('Decreased Vitality');
+  console.log(vitality);
+}
 
-  // Get Hero Body Parts
-    let gladHead = document.getElementById("gladiatorHead");
+// Subtract Agility
+function subtractAgi (e) {
+  const agilityBar = document.getElementById('agiBar');
+  e.preventDefault();
 
-    let samHead = document.getElementById("samuraiHead");
-    let samBody = document.getElementById("samuraiBody");
+  if (agility <= 0) {
+    agilityBar.style.display = 'none';
+    agility = 0;
+    console.log('Zero Agility');
+    return;
+  }
 
-    let ninja = document.getElementById("ninja");
+  agility -= 3;
+  agilityBar.style.display = 'inline';
+  agilityBar.style.width = agility + '%';
+  agilityBar.style.opacity = '80%';
+  console.log('Decreased Agility');
+  console.log(agility);
+}
 
-// --- TOGGLE HERO DISPLAY FUNCTIONS ---
+// Subtract Intelligence
+function subtractInt (e) {
+  const intelligenceBar = document.getElementById('intBar');
+  e.preventDefault();
 
-  // Toggle Gladiator display
-    const gladiatorBtn = document.getElementById("gladiatorBtn");
+  if (intelligence <= 0) {
+    intelligenceBar.style.display = 'none';
+    intelligence = 0;
+    console.log('Zero Intelligence');
+    return;
+  }
 
-    gladiatorBtn.addEventListener('click', function toggleGladiator() {
-      gladHead.style.display = "inline-block";
+  intelligence -= 3;
+  intelligenceBar.style.display = 'inline';
+  intelligenceBar.style.width = intelligence + '%';
+  console.log('Decreased Intelligence');
+  console.log(intelligence);
+}
 
-      samurai.style.display = "none";
+// Subtract Luck
+function subtractLuk (e) {
+  const luckBar = document.getElementById('lukBar');
+  e.preventDefault();
 
-      ninja.style.display = "none";
+  if (luck <= 0) {
+    luckBar.style.display = 'none';
+    luck = 0;
+    console.log('Zero Luck');
+    return;
+  }
 
-    }, false);
+  luck -= 3;
+  luckBar.style.display = 'inline';
+  luckBar.style.width = luck + '%';
+  console.log('Decreased Luck');
+  console.log(luck);
+}
 
-  // Toggle Ninja display
-    const ninjaBtn = document.getElementById("ninjaBtn");
-    
-    ninjaBtn.addEventListener('click', function toggleNinja() {
-      ninja.style.display = "inline-block";
-      
-      gladHead.style.display = "none";
+// _________________ Choose Hero Display ; Depends on Selected Class _________________ //
 
-      samurai.style.display = "none";
-      
-    }, false);
+// Get Heros
+const gladiator = document.getElementById('gladiator');
+const samurai = document.getElementById('samurai');
+const ninja = document.getElementById('ninja');
 
-  // Toggle Samurai display
-    const samuraiBtn = document.getElementById("samuraiBtn");
-    
-    samuraiBtn.addEventListener('click', function toggleSamurai() {
-      samurai.style.display = "inline-block";
+// --- TOGGLE HERO FUNCTIONS --- //
 
-      gladHead.style.display = "none";
+// Toggle Gladiator display
+const gladiatorBtn = document.getElementById('gladiatorBtn');
 
-      ninja.style.display = "none";
+gladiatorBtn.addEventListener('click', function toggleGladiator () {
+  gladiator.style.display = 'inline-block';
 
-    }, false);
+  samurai.style.display = 'none';
+
+  ninja.style.display = 'none';
+}, false);
+
+// Toggle Ninja display
+const ninjaBtn = document.getElementById('ninjaBtn');
+
+ninjaBtn.addEventListener('click', function toggleNinja () {
+  ninja.style.display = 'inline-block';
+
+  gladiator.style.display = 'none';
+
+  samurai.style.display = 'none';
+}, false);
+
+// Toggle Samurai display
+const samuraiBtn = document.getElementById('samuraiBtn');
+
+samuraiBtn.addEventListener('click', function toggleSamurai () {
+  samurai.style.display = 'inline-block';
+
+  gladiator.style.display = 'none';
+
+  ninja.style.display = 'none';
+}, false);
